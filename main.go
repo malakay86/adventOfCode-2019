d3032c1ab149a015003f46797a9cc2f96de12e6a
vmas <victor.arc2@gmail.com>
2019-12-01 19:10:04 +0100

    Day 1

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "strconv"
+)
+
+func calculateFuelForMass(mass int) (fuel int) {
+  fuel = (mass / 3) - 2
+  return
+}
+
+
+func calculateFuelForFuel (fuel int) int {
+  var totalFuel, currentFuel = 0,fuel
+
+  for currentFuel > 0 {
+    fuelForFuel := calculateFuelForMass(currentFuel)
+    if fuelForFuel > 0 {
+      totalFuel += fuelForFuel
+    }
+    currentFuel = fuelForFuel
+  }
+
+  return totalFuel
+}
+
+func main() {
+  // Read input file
+  file, err := os.Open("input.txt")
+  if err != nil {
+      fmt.Println("Error opening input file")
+  }
+  defer file.Close()
+
+  // Read line by line to perform calculation
+  scanner := bufio.NewScanner(file)
+  accumulated := 0
+  for scanner.Scan() {
+      if massAsInt, err := strconv.Atoi(scanner.Text()); err == nil {
+        fuelNeededForMass := calculateFuelForMass(massAsInt)
+
+        // Accumulate the fuel for the mass
+        accumulated += fuelNeededForMass
+
+        // Calculate fuel needed for the fuel itself
+        if fuelNeededForMass > 0 {
+          accumulated += calculateFuelForFuel(fuelNeededForMass)
+        }
+      }
+  }
+
+  fmt.Println("Total fuel amount needed:", accumulated)
+}
