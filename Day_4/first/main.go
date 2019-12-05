d3b3659758dda6f9c539f82eb060b8b212d701c2
Víctor Arcos <victorarcos@protonmail.com>
2019-12-05 15:38:03 +0100

    Update main.go

--- Day_4/first/main.go
+++ Day_4/first/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -18,17 +19,53 @@ func SixDigitNumber(num int) bool {
 
 func HasTwoRepeatedAdjacentDigits(num int64) bool {
 	numAsStr := strconv.FormatInt(num, 10)
+	result := false
 	var previousDigit rune
+	counter := 1
 	for _, digit := range numAsStr {
 		if previousDigit == digit {
-			return true
+			if counter > 2 {
+				return false
+			}
+
+			counter++
+			result = true
+		} else {
+			counter = 1
 		}
+
 		previousDigit = digit
 	}
 
-	return false
+	return result
+}
+
+func DigitsNeverDecrease (num int64) bool {
+	numAsStr := strconv.FormatInt(num, 10)
+	var previousDigit int32 = -1;
+
+	for _, digit := range numAsStr {
+		if previousDigit <= digit {
+			previousDigit = digit
+		} else {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
+	min := 387638;
+	max := 919123;
 
+	count := 0
+	for i := min; i <= max; i++ {
+		if SixDigitNumber(i) &&
+			HasTwoRepeatedAdjacentDigits(int64(i)) &&
+			DigitsNeverDecrease(int64(i)) {
+			count++
+		}
+	}
+
+	fmt.Print(count)
 }
